b8dcf742ded11f70407e183e0422718be5024d96
Kelum nagodavithana <54311397+sanjuTec@users.noreply.github.com>
2021-07-04 15:40:49 +0530

    Initial commit

---
+++ Calculator/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "0" || isOperationPerformed) {
+                textBox_result.Clear();
+            }
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+
+            if (button.Text == ".")
+            {
+                if (!textBox_result.Text.Contains("."))
+                    textBox_result.Text = textBox_result.Text + button.Text;
+
+            }
+            else 
+                textBox_result.Text = textBox_result.Text + button.Text;
+                
+            
+            
+            
+            
+        }
+
+        private void textBox_result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operatorClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (resultValue != 0)
+            {
+                eqlBtn.PerformClick();
+                operationPerformed = button.Text;
+                labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+
+            }
+            else { 
+                operationPerformed = button.Text;
+                resultValue = Double.Parse(textBox_result.Text);
+                labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+
+        }
+         
+        private void ClearClick(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+        }
+
+        private void ClikClear2(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+            resultValue = 0;
+        }
+
+        private void clickEqual(object sender, EventArgs e)
+        {
+            switch (operationPerformed) {
+                case "+":
+                    textBox_result.Text = (resultValue + Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_result.Text = (resultValue - Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "*":
+                    textBox_result.Text = (resultValue * Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "/":
+                    textBox_result.Text = (resultValue / Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+
+            resultValue = Double.Parse(textBox_result.Text);
+            labelCurrentOperation.Text = "";
+        }
+    }
+}
